dc936dff9b23992002abbb5ad4feaa4f097a8906
nerdkitty1988 <jamiarnold2284@gmail.com>
2021-09-09 18:55:14 -0500

    Styled login

---
+++ frontend/src/components/LoginFormPage/index.js
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+import * as sessionActions from "../../store/session";
+import { useDispatch, useSelector } from "react-redux";
+import { NavLink, Redirect } from "react-router-dom";
+import "./LoginForm.css";
+
+function LoginFormPage() {
+	const dispatch = useDispatch();
+	const sessionUser = useSelector((state) => state.session.user);
+	const [credential, setCredential] = useState("");
+	const [password, setPassword] = useState("");
+	const [errors, setErrors] = useState([]);
+
+	if (sessionUser) return <Redirect to="/" />;
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		setErrors([]);
+		return dispatch(sessionActions.login({ credential, password })).catch(
+			async (res) => {
+				const data = await res.json();
+				if (data && data.errors) setErrors(data.errors);
+			}
+		);
+	};
+
+	return (
+		<div className="signInBox">
+			<div className="loginTop">
+				<h1 className="loginHead">Log in</h1>
+				<p>Not registered with us yet? Sign up</p>
+			</div>
+			<div className="signinForm">
+				<form onSubmit={handleSubmit}>
+					<ul>
+						{errors.map((error, idx) => (
+							<li key={idx}>{error}</li>
+						))}
+					</ul>
+					<div className="signInFields">
+						<label className="login-label">
+							Username or Email
+							<input
+								className="signInInput"
+								type="text"
+								value={credential}
+								onChange={(e) => setCredential(e.target.value)}
+								required
+							/>
+						</label>
+						<label className="login-label" htmlFor="password" />
+							Password
+							<NavLink to="/forgotPassword">
+								Forgot your password?
+							</NavLink>
+							<input
+								className="signInInput"
+                                name="password"
+								type="password"
+								value={password}
+								onChange={(e) => setPassword(e.target.value)}
+								required
+							/>
+						<button type="submit" className="loginButton">
+							Log In
+						</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	);
+}
+
+export default LoginFormPage;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,13 +7,16 @@ import App from "./App";
 import configureStore from "./store";
 import { restoreCSRF, csrfFetch } from "./store/csrf";
 
+import * as sessionActions from './store/session';
+
 const store = configureStore();
 
-if (process.env.NODE_ENV !== "production") {
-	restoreCSRF();
+if (process.env.NODE_ENV !== 'production') {
+  restoreCSRF();
 
-	window.csrfFetch = csrfFetch;
-	window.store = store;
+  window.csrfFetch = csrfFetch;
+  window.store = store;
+  window.sessionActions = sessionActions;
 }
 
 function Root() {
