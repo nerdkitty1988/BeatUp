f8d363258863e73e6b80b94ff284f5d469beeb84
nerdkitty1988 <jamiarnold2284@gmail.com>
2021-09-19 19:29:37 -0500

    RSVP finished

--- frontend/src/components/SingleEventPage/index.js
+++ frontend/src/components/SingleEventPage/index.js
@@ -3,84 +3,105 @@ import { useDispatch, useSelector } from "react-redux";
 import { NavLink, useParams, useHistory } from "react-router-dom";
 import "./SingleEventPage.css";
 
-
 import { getEvents, updateEvent, deleteEvent } from "../../store/event";
-import { getRsvps } from "../../store/rsvp";
 import { getLocations } from "../../store/location";
-
+import { createRsvp } from "../../store/rsvp";
 
 const SingleEventPage = () => {
-    const sessionUser = useSelector((state) => state.session.user);
+	const sessionUser = useSelector((state) => state.session.user);
 	const dispatch = useDispatch();
 	const { eventId } = useParams();
-    let history = useHistory();
+	let history = useHistory();
 
-    const event = useSelector(state => state.eventState.eventList[eventId]);
-
-	const rsvps = useSelector((state) => {
-        return Object.values(state.rsvpState.rsvpList);
-	});
+	const event = useSelector((state) => state.eventState.eventList[eventId]);
 	const locations = useSelector((state) => {
-        return Object.values(state.locationState.locationList);
+		return Object.values(state.locationState.locationList);
 	});
 
+	let rsvps;
+	let location;
+
+	if (event) {
+		rsvps = event.Rsvps.filter((rsvp) => {
+			return rsvp.userId === sessionUser.id;
+		})[0];
+		location = event.Location;
+	}
 
 	const [eventName, setEventName] = useState(event ? event.eventName : "");
-	const [eventLocationId, setEventLocationId] = useState(event ? locations[event.eventLocationId] : "");
-	const [eventDate, setEventDate] = useState(event ? event.eventDate : new Date());
+	const [eventLocationId, setEventLocationId] = useState(
+		event ? locations[event.eventLocationId] : ""
+	);
+	const [eventDate, setEventDate] = useState(
+		event ? event.eventDate : new Date()
+	);
 	const [eventTime, setEventTime] = useState(event ? event.eventTime : "");
-	const [eventDescription, setEventDescription] = useState(event ? event.eventDescription : "");
-	const [eventPhotoUrl, setEventPhotoUrl] = useState(event ? event.eventPhotoUrl : "https://www.google.com/url?sa=i&url=https%3A%2F%2Fcommonlook.com%2Ftribe-related-events-placeholder%2F&psig=AOvVaw2KIYVmzCCF1e8p8Hyfj_Px&ust=1631898035505000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCKjD04_8g_MCFQAAAAAdAAAAABAE");
+	const [eventDescription, setEventDescription] = useState(
+		event ? event.eventDescription : ""
+	);
+	const [eventPhotoUrl, setEventPhotoUrl] = useState(
+		event
+			? event.eventPhotoUrl
+			: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fcommonlook.com%2Ftribe-related-events-placeholder%2F&psig=AOvVaw2KIYVmzCCF1e8p8Hyfj_Px&ust=1631898035505000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCKjD04_8g_MCFQAAAAAdAAAAABAE"
+	);
 	const [eventOwnerId, setEventOwnerId] = useState();
 	const [groupId, setGroupId] = useState();
+    const [rsvpStatus, setRsvpStatus] = useState();
 	const [errors, setErrors] = useState([]);
 	const [showEdit, setShowEdit] = useState(true);
 
-    const readDate = event ? (new Date(event.eventDate).toDateString()) : new Date();
+	const readDate = event
+		? new Date(event.eventDate).toDateString()
+		: new Date();
 
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		setErrors([]);
+		const payload = {
+			id: eventId,
+			eventName,
+			eventLocationId,
+			eventDate,
+			eventTime,
+			eventDescription,
+			eventPhotoUrl,
+		};
+		let updatedEvent = await dispatch(updateEvent(payload));
+		if (updatedEvent) {
+			setShowEdit(true);
+		}
+	};
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        setErrors([]);
-        const payload = {
-            id: eventId,
-            eventName,
-            eventLocationId,
-            eventDate,
-            eventTime,
-            eventDescription,
-            eventPhotoUrl,
-        };
-        let updatedEvent = await dispatch(updateEvent(payload))
-        if (updatedEvent) {
-            setShowEdit(true);
+	useEffect(() => {
+		dispatch(getEvents());
+	}, [dispatch]);
+
+	useEffect(() => {
+		dispatch(getLocations());
+	}, [dispatch]);
+
+	const handleDelete = async (e) => {
+		e.preventDefault();
+		await dispatch(deleteEvent(event));
+		history.push("/events");
+	};
+
+	const handleCancelClick = (e) => {
+		e.preventDefault();
+		setShowEdit(true);
+	};
+
+    const handleCreateRsvp = () => {
+        const newRsvp = {
+            userId: sessionUser.id,
+            eventId: event.id,
+            rsvpStatus
         }
+        history.push(`/events`)
+        dispatch(createRsvp(newRsvp))
     };
 
-    useEffect(() => {
-        dispatch(getEvents());
-    }, [dispatch]);
-
-    useEffect(() => {
-        dispatch(getRsvps());
-    }, [dispatch]);
-
-    useEffect(() => {
-        dispatch(getLocations());
-    }, [dispatch]);
-    const handleDelete = async (e) => {
-        e.preventDefault();
-        await dispatch(deleteEvent(event));
-        history.push('/events');
-    };
-
-    const handleCancelClick = (e) => {
-        e.preventDefault();
-        setShowEdit(true)
-    }
-
-    if (!event) return null;
-
+	if (!event) return null;
 
 	return (
 		<div id="wholeEvent">
@@ -104,10 +125,8 @@ const SingleEventPage = () => {
 						Location
 						<select
 							id="locations"
-                            value={eventLocationId}
-							onChange={(e) =>
-								setEventLocationId(e.target.value)
-							}
+							value={eventLocationId}
+							onChange={(e) => setEventLocationId(e.target.value)}
 						>
 							{locations.map((location, idx) => (
 								<option key={location.id} value={location.id}>
@@ -124,8 +143,9 @@ const SingleEventPage = () => {
 							className="editEventInput"
 							value={eventDate}
 							onChange={(e) => {
-                                let updatedDate = e.target.value;
-                                setEventDate(updatedDate)}}
+								let updatedDate = e.target.value;
+								setEventDate(updatedDate);
+							}}
 						/>
 					</label>
 					<label>
@@ -159,21 +179,26 @@ const SingleEventPage = () => {
 						/>
 					</label>
 					<button type="submit">Submit</button>
-                    <button type="button" onClick={handleCancelClick}>Cancel</button>
+					<button type="button" onClick={handleCancelClick}>
+						Cancel
+					</button>
 				</form>
 			</div>
 			<div className="singleEventCont">
 				<div className="eventCont">
 					<div id="editEventNav">
 						<button
+							className="singleEventButtons"
 							onClick={() => setShowEdit(true ? false : true)}
 						>
 							Edit Event
 						</button>
-                        <button
-                            onClick={handleDelete}>
-                                Delete Event
-                        </button>
+						<button
+							className="singleEventButtons"
+							onClick={handleDelete}
+						>
+							Delete Event
+						</button>
 					</div>
 					<img
 						id="eventPhoto"
@@ -200,13 +225,15 @@ const SingleEventPage = () => {
 					<p key="eventDesc">{event.eventDescription}</p>
 					<p key="eventDate">{readDate}</p>
 					<p key="eventTime">{event.eventTime}</p>
-					{rsvps.map((rsvp) => {
-						if (
-							rsvp.userId === sessionUser.id &&
-							rsvp.eventId === event.id
-						)
-							return <p key={rsvp.id}>{rsvp.rsvpStatus}</p>;
-					})}
+					{rsvps ? (
+						<p>{rsvps.rsvpStatus}</p>
+					) : (
+						<select id="rsvpChoice" onChange={(e) => setEventName(e.target.value), handleCreateRsvp}>
+							<option value="Attending">Attending</option>
+							<option value="Not Attending">Not Attending</option>
+							<option value="Maybe">Maybe</option>
+						</select>
+					)}
 				</div>
 			</div>
 		</div>
